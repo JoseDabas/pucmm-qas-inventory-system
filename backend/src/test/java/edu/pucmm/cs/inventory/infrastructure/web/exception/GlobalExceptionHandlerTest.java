7f7a21ed69f554ae87a0d7c1728aaa8cba65eacb
Jose Ariel <josearieldabas01@gmail.com>
2026-07-12 18:35:13 -0400

    test: cubrir handler de rutas no encontradas para satisfacer coverage de SonarQube

--- backend/src/test/java/edu/pucmm/cs/inventory/infrastructure/web/exception/GlobalExceptionHandlerTest.java
+++ backend/src/test/java/edu/pucmm/cs/inventory/infrastructure/web/exception/GlobalExceptionHandlerTest.java
@@ -10,12 +10,14 @@ import static org.mockito.Mockito.when;
 import org.springframework.beans.TypeMismatchException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpInputMessage;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import jakarta.persistence.EntityNotFoundException;
 
@@ -26,7 +28,8 @@ class GlobalExceptionHandlerTest {
         GlobalExceptionHandler handler = new GlobalExceptionHandler();
 
         // 1. HttpMessageNotReadableException
-        HttpMessageNotReadableException ex1 = new HttpMessageNotReadableException("error", mock(HttpInputMessage.class));
+        HttpMessageNotReadableException ex1 = new HttpMessageNotReadableException("error",
+                mock(HttpInputMessage.class));
         ProblemDetail pd1 = handler.handleHttpMessageNotReadableException(ex1);
         assertEquals(HttpStatus.BAD_REQUEST.value(), pd1.getStatus());
 
@@ -54,21 +57,27 @@ class GlobalExceptionHandlerTest {
         assertEquals(HttpStatus.FORBIDDEN.value(), pd6.getStatus());
 
         // 7. DataIntegrityViolationException
-        ProblemDetail pd7 = handler.handleDataIntegrityViolationException(new DataIntegrityViolationException("integrity"));
+        ProblemDetail pd7 = handler
+                .handleDataIntegrityViolationException(new DataIntegrityViolationException("integrity"));
         assertEquals(HttpStatus.CONFLICT.value(), pd7.getStatus());
 
         // 8. HttpRequestMethodNotSupportedException (Rama null y Rama válida)
         HttpRequestMethodNotSupportedException ex8 = new HttpRequestMethodNotSupportedException("POST", List.of("GET"));
         var response = handler.handleHttpRequestMethodNotSupportedException(ex8);
         assertEquals(HttpStatus.METHOD_NOT_ALLOWED, response.getStatusCode());
-        
+
         HttpRequestMethodNotSupportedException ex8Null = mock(HttpRequestMethodNotSupportedException.class);
         when(ex8Null.getSupportedHttpMethods()).thenReturn(null);
         var responseNull = handler.handleHttpRequestMethodNotSupportedException(ex8Null);
         assertNotNull(responseNull);
 
-        // 9. Exception general
-        ProblemDetail pd9 = handler.handleException(new Exception("generic"));
-        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR.value(), pd9.getStatus());
+        // 9. NoResourceFoundException / NoHandlerFoundException -> 404
+        NoResourceFoundException ex9 = new NoResourceFoundException(HttpMethod.GET, "/ruta-inexistente");
+        ProblemDetail pd9 = handler.handleNotFoundException(ex9);
+        assertEquals(HttpStatus.NOT_FOUND.value(), pd9.getStatus());
+
+        // 10. Exception general
+        ProblemDetail pd10 = handler.handleException(new Exception("generic"));
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR.value(), pd10.getStatus());
     }
 }
