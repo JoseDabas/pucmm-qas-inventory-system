c094ab8e3a1a3e8f93b6ac870451b2ff031c5032
Jose Ariel <josearieldabas01@gmail.com>
2026-07-30 10:55:00 -0400

    fix(performance): fijar k6-reporter a la versión 2.4.0 para resolver el SyntaxError que rompía el stage de k6 en el pipeline

--- performance/api-performance.js
+++ performance/api-performance.js
@@ -1,6 +1,6 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
-import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/2.4.0/dist/bundle.js";
 import { textSummary } from "https://jslib.k6.io/k6-summary/0.1.0/index.js";
 
 /**
